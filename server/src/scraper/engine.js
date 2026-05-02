3af77c0a4674050ec8082115e9c185aa9e42db09
AzizzAzizli <azizliaziz025@gmail.com>
2026-05-03 02:42:44 +0400

    raiway build

--- server/src/scraper/engine.js
+++ server/src/scraper/engine.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer");
 async function scrapeAZTU(username, password) {
   const browser = await puppeteer.launch({
     headless: "new",
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
